7b12719d420b13ccffa296aba04591237d6b820b
GhadeerHabeeb <ghadeerhabeeb2@gmail.com>
2022-10-01 15:53:43 +0300

    changes

---
+++ script.js
@@ -0,0 +1,14 @@
+const settingsMenu=document.querySelector('.settings-menu');
+const darkBtn=document.getElementById('dark-btn');
+
+
+function settingsMenuToggle()
+{   console.log('clicked')
+    settingsMenu.classList.toggle("settings-menu-height");
+}
+
+darkBtn.onclick=function()
+{
+    darkBtn.classList.toggle("dark-btn-on");
+    document.body.classList.toggle('dark-theme');
+}
